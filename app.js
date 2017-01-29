b371b50c4afa94215ae53d0cf05876888be09d4f
Sinrok Kim <fresh502@gmail.com>
2017-01-29 14:10:37 +0900

    fix: fix convention

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/index', function (req, res) {
-  res.send('Hello index!')
-})
+app.get('/index', (req, res) => {
+  res.send('Hello index!');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
